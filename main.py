7751f56246a2d39e44e184d2f64e1c4276baa251
Sekou <Sekoutraore750@yahoo.com>
2026-01-26 23:20:30 -0500

    Slowing down for style

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+# Author: Sekou Traore
+# Network monitoring script
+# Make a minimum working version and incrementally improve
 
+
+# interesting library that provides system and network information
 import psutil
+import time
 
+
+# Returns system-wide network I/O statistics as a named tuple(pernic=True for per network interface)
 network_stats = psutil.net_io_counters(pernic=False, nowrap=True)
+
+print("Welcome to my network monitoring script")
+time.sleep(1)
+print("Gathering network statistics...")
+time.sleep(3)
 print(network_stats)
